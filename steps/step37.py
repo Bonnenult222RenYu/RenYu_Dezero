b78c6024cdf67f8adba6357594b1d51dc0b390a7
liangxiaocong <1649761717@qq.com>
2024-02-25 23:58:15 +0800

    2024-02-25
    steps/step35.py dezero/functions.py: add tanh
    steps/step36.py: double backprop
    steps/step37.py: Processing Tensor but not implemented F.sum
    steps/step38.py dezero/functions.py dezero/core.py: Functions that change shape

---
+++ steps/step37.py
@@ -0,0 +1,21 @@
+# Add import path for the dezero directory.
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import numpy as np
+from dezero import Variable
+from dezero.utils import plot_dot_graph
+import dezero.functions as F
+
+
+x = Variable(np.array([[1, 2, 3], [4, 5, 6]]))
+c = Variable(np.array([[10, 20, 30], [40, 50, 60]]))
+t = x + c
+# y = F.sum(t) # NotImplemented
+
+# y.backward(retain_grad=True)
+# print(y.grad)
+# print(t.grad)
+# print(x.grad)
+# print(c.grad)
