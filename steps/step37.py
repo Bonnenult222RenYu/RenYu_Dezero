9ba82119f913701a6d5099875e6d0722407a3ed7
renyu <1649761717@qq.com>
2024-02-27 23:52:03 +0800

    2024-02-27
    steps/step39.py dezero/functions.py dezero/utils.py: 求和的函数
    steps/step40.py dezero/functions.py dezero/core.py: 进行广播的函数

--- steps/step37.py
+++ steps/step37.py
@@ -12,10 +12,10 @@ import dezero.functions as F
 x = Variable(np.array([[1, 2, 3], [4, 5, 6]]))
 c = Variable(np.array([[10, 20, 30], [40, 50, 60]]))
 t = x + c
-# y = F.sum(t) # NotImplemented
+y = F.sum(t)
 
-# y.backward(retain_grad=True)
-# print(y.grad)
-# print(t.grad)
-# print(x.grad)
-# print(c.grad)
+y.backward(retain_grad=True)
+print(y.grad)
+print(t.grad)
+print(x.grad)
+print(c.grad)
