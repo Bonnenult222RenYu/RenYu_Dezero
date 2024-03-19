b8b57864d69aae4ea265470515eb84b2a91606b0
renyu <1649761717@qq.com>
2024-03-19 22:53:13 +0800

    2024-03-19
    steps/step50.py dezero/functions.py dezero/dataloaders.py dezero/__init__.py : DataLoader for retrieving small batches of data 用于取出小批量数据的 DataLoader

--- dezero/functions.py
+++ dezero/functions.py
@@ -1,5 +1,5 @@
 import numpy as np
-from dezero.core import Function, as_variable
+from dezero.core import Function, Variable, as_variable, as_array
 from dezero import utils
 
 # =============================================================================
@@ -442,3 +442,19 @@ class Clip(Function):
 
 def clip(x, x_min, x_max):
     return Clip(x_min, x_max)(x)
+
+
+# =============================================================================
+# accuracy / dropout / batch_norm / embed_id
+# =============================================================================
+def accuracy(y, t):
+    """
+    [WAR] This function is not differentiable.
+    """
+    y, t = as_variable(y), as_variable(t)
+
+    pred = y.data.argmax(axis=1).reshape(t.shape)
+    result = (pred == t.data)
+    acc = result.mean()
+    return Variable(as_array(acc))
+
